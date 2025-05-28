e59c4f6f4064bd11e93bb219c4c8f861efa792d9
Vishakha Baghel <163894187+Vishakha-23@users.noreply.github.com>
2025-05-28 11:02:39 +0530

    made changes

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 console.log("script.js initializing")
 
-// let vname = document.getElementsByClassName(".vName")
-// let cname = document.getElementsByClassName(".cName")
-// let viewNo = document.getElementsByClassName(".views")
-// let mon = document.getElementsByClassName(".month")
-// let thumb = document.getElementsByClassName(".thumbnail")
 
 function createCard(title, cName, views, monthsOld, duration, thumbnail){
     // Finish this function
@@ -15,8 +10,8 @@ function createCard(title, cName, views, monthsOld, duration, thumbnail){
     document.getElementById("duratn").textContent = duration ;
     document.getElementById("myImage").src = thumbnail;
 }
-    // document.getElementById("myImage").src = "https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
+    
 
 
-createCard("Introduction to Backend | Sigma Web Dev video #2", "CodeWithHarry", "560000", "7", "31:22", "https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" )
+createCard("Introduction to Backend | Sigma Web Dev video #2", "CodeWithHarry", "560000", "7", "31:22", "thumbnail.jpeg" )
 
